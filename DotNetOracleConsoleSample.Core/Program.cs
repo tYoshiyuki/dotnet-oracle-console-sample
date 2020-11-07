1050f96d4952a107490a545b6addfe10dc3c9d18
yTsunomoto <wasiwasimaru@gmail.com>
2020-11-08 00:58:24 +0900

    update 更新。

--- DotNetOracleConsoleSample.Core/Program.cs
+++ DotNetOracleConsoleSample.Core/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNetOracleConsoleSample.Core.Extensions;
 using DotNetOracleConsoleSample.Core.Models;
 
 namespace DotNetOracleConsoleSample.Core
@@ -9,7 +11,8 @@ namespace DotNetOracleConsoleSample.Core
         {
             // TODO 設定化する
             var db = new OracleDbContext("User Id=sample;Password=samplepw;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))");
-            var blog = db.Blogs.ToList();
+            // var blog = db.Blogs.ToList();
+            var blog = db.SqlQuery<Blog>("SELECT * FROM SAMPLE.\"Blogs\"").ToList();
         }
     }
 }

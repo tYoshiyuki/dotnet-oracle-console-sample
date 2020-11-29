7c496e3082d8068ffdb9a34de188fd83d1e59e30
yTsunomoto <wasiwasimaru@gmail.com>
2020-11-29 22:35:04 +0900

    update 更新。

---
+++ DotNetOracleConsoleSample.DotNetFive/Models/OracleDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetOracleConsoleSample.DotNetFive.Models
+{
+    public class OracleDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public OracleDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle(_connectionString);
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+    }
+}
--- DotNetOracleConsoleSample.DotNetFive/Program.cs
+++ DotNetOracleConsoleSample.DotNetFive/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Linq;
-using DotNetOracleConsoleSample.Core.Extensions;
-using DotNetOracleConsoleSample.Core.Models;
+using DotNetOracleConsoleSample.DotNetFive.Extensions;
+using DotNetOracleConsoleSample.DotNetFive.Models;
 
-namespace DotNetOracleConsoleSample.Core
+namespace DotNetOracleConsoleSample.DotNetFive
 {
     class Program
     {
@@ -11,8 +11,8 @@ namespace DotNetOracleConsoleSample.Core
         {
             // TODO 設定化する
             var db = new OracleDbContext("User Id=sample;Password=samplepw;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))");
-
             var blog = db.Blogs.ToList();
+
             Console.WriteLine("----- get data from db (ef core model) -----");
             blog.ForEach(x => Console.WriteLine($"{x.Id}:{x.Name}"));
             Console.WriteLine("----------");

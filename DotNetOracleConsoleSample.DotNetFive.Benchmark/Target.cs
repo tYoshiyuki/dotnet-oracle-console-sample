9f8829614be3bb3d6f54b18a87432c825192c77c
yTsunomoto <wasiwasimaru@gmail.com>
2020-11-29 23:15:07 +0900

    update 更新。

---
+++ DotNetOracleConsoleSample.DotNetFive.Benchmark/Target.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using DotNetOracleConsoleSample.DotNetFive.Extensions;
+using DotNetOracleConsoleSample.DotNetFive.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetOracleConsoleSample.DotNetFive.Benchmark
+{
+    public class Target
+    {
+        private readonly int loopCnt = 1000;
+
+        [Benchmark]
+        public void SqlQuery()
+        {
+            var db = new OracleDbContext("User Id=sample;Password=samplepw;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))");
+
+            var i = 0;
+            while (true)
+            {
+                var result = db.SqlQuery<Blog>("SELECT * FROM SAMPLE.\"Blogs\"").ToList();
+                i++;
+                if (i > loopCnt) break;
+            }
+        }
+
+        [Benchmark]
+        public void FromSqlRaw()
+        {
+            var db = new OracleDbContext("User Id=sample;Password=samplepw;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))");
+
+            var i = 0;
+            while (true)
+            {
+                var result = db.Blogs.FromSqlRaw("SELECT * FROM SAMPLE.\"Blogs\"").ToList();
+                i++;
+                if (i > loopCnt) break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -16,3 +16,18 @@ ODP.NET で Oracle にデータアクセスするサンプル
     - DotNetOracleConsoleSample.Core のベンチマーク用
 - DotNetOracleConsoleSample.DotNetFive
     - ODP.NET + .NET5 + EntityFrameworkCore のサンプル
+- DotNetOracleConsoleSample.DotNetFive.Benchmark
+    - DotNetOracleConsoleSample.DotNetFive.Benchmark のベンチマーク用
+
+## Benchmark Result
+### DotNetOracleConsoleSample.Core.Benchmark
+|     Method |       Mean |    Error |   StdDev |
+|----------- |-----------:|---------:|---------:|
+|   SqlQuery | 1,098.5 ms | 20.67 ms | 31.56 ms |
+| FromSqlRaw |   775.6 ms | 15.36 ms | 28.08 ms |
+
+### DotNetOracleConsoleSample.DotNetFive.Benchmark
+|     Method |       Mean |    Error |   StdDev |
+|----------- |-----------:|---------:|---------:|
+|   SqlQuery | 1,129.0 ms | 21.05 ms | 22.52 ms |
+| FromSqlRaw |   789.2 ms | 15.67 ms | 31.30 ms |

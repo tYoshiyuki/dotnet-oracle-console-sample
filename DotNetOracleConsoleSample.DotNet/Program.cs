6d122362914ac978d762b6ea55a14ce95e12d659
Yoshiyuki Tsunomoto <wasiwasimaru@gmail.com>
2024-05-06 11:25:12 +0900

    update 更新。

--- DotNetOracleConsoleSample.DotNet/Program.cs
+++ DotNetOracleConsoleSample.DotNet/Program.cs
@@ -10,7 +10,7 @@ class Program
     static void Main(string[] args)
     {
         // TODO 設定化する
-        var db = new OracleDbContext("User Id=sample;Password=samplepw;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)))");
+        var db = new OracleDbContext("User Id=sample;Password=samplepw;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XEPDB1)))");
         var blog = db.Blogs.ToList();
 
         Console.WriteLine("----- get data from db (ef core model) -----");
